a5eadc1bdb4dd2035e7fc72e2425222df3334e40
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-25 10:23:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const parseCode = (str) => {
   // your code here
+	let arr = str.split("0");
+	  let obj = {};
+	  let i = 0;
+	  arr.forEach(e => {
+		if(e !== ""){
+			if(i===0){
+				obj.firstName = e;
+			}else if(i===1){
+				obj.lastName = e
+			}else{
+				obj.id = Number(e);
+			}
+			i++;     
+		}
+	  });
+	  //console.log(obj);
+	  return obj;
 };
-
 // Do not change the code below
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
