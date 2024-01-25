7aa8908a0be1395b422d9d5be844b5d392fe9cb3
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-25 10:47:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const parseCode = (str) => {
   // your code here
-	let arr = str.split("0");
-	  let obj = {};
-	  let i = 0;
-	  arr.forEach(e => {
-		if(e !== ""){
-			if(i===0){
-				obj.firstName = e;
-			}else if(i===1){
-				obj.lastName = e
-			}else{
-				obj.id = e;
-			}
-			i++;     
-		} 
-	  });
-	  return obj;
+	let arr = str.split('0');
+	let filteredArray = arr.filter(str => str !== '');
+	let result = { firstName: filteredArray[0], lastName: filteredArray[1], id: filteredArray[2] };
+	return result;
+	 //  let obj = {};
+	 //  let i = 0;
+	 //  arr.forEach(e => {
+		// if(e !== ""){
+		// 	if(i===0){
+		// 		obj.firstName = e;
+		// 	}else if(i===1){
+		// 		obj.lastName = e
+		// 	}else{
+		// 		obj.id = e;
+		// 	}
+		// 	i++;     
+		// } 
+	 //  });
+	 //  return obj;
 };
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str))); 
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str))); 
